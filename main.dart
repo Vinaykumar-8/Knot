060bb844fe7b17c3b4d8eba17365ea8da204824e
Vinaykumar-8 <vinay17022005@gmail.com>
2026-03-15 23:59:25 +0530

    Fix import formatting in main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-import 'dart:convert'
+import 'dart:convert';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
